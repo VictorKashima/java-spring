e1b81afa3ea270f0024b052693062f86f02d6acf
ゆいち <victorkashima04@gmail.com>
2023-05-16 17:30:35 +0000

    FIX: Crud livro

--- app/src/main/java/application/model/Livro.java
+++ app/src/main/java/application/model/Livro.java
@@ -13,17 +13,26 @@ public class Livro {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String titulo;
+    private String isbn;
 
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
-    }
     public String getTitulo() {
         return titulo;
     }
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
-    }   
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
 }
